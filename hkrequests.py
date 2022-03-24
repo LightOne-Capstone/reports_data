d2166b0281f8bb866082de937eb84b7f5b05d1a9
junhoLee <plusjob70@gmail.com>
2022-03-24 21:32:05 +0900

    backup

--- hkrequests.py
+++ hkrequests.py
@@ -8,24 +8,24 @@ class HKRequests:
     # 날짜 포맷 : 2022-01-01
     # _sdate : start date(시작날짜)
     # _edate : end date(종료날짜)
+    # 투자 의견 : [BUY, HOLD, NR, OUTPERFORM, REDUCE, STRONGBUY, SUSPENDED, TRADINGBUY, UNDERPERFORM, ...]
     def __init__(self, _sdate: str, _edate: str):
         self.target_corp = {'대신증권', '유안타증권', '유진투자증권', '키움증권', '한양증권', '하이투자증권', '한화투자증권'}
-        self.suggestion_label = ['BUY', 'HOLD', 'NR', 'OUTPERFORM', 'REDUCE', 'STRONGBUY',
-                                 'SUSPENDED', 'TRADINGBUY', 'UNDERPERFORM']
-        self.suggestion_correction = [('-', 'NR'), ('NOTRATED', 'NR'), ('NA', 'NR'), ('N/A', 'NR'),
-                                      ('중립', 'HOLD'), ('매수', 'BUY'), ('MARKETPERFORM', 'HOLD'),
-                                      ('NEUTRAL', 'HOLD'), ('적극매수', 'STRONGBUY'), ('투자의견없음', 'NR')]
+        self.suggestion_correction = {'-': 'NR', 'NOTRATED': 'NR', 'NA': 'NR', 'N/A': 'NR', '중립': 'HOLD',
+                                      '매수': 'BUY', 'MARKETPERFORM': 'HOLD', 'NEUTRAL': 'HOLD',
+                                      '적극매수': 'STRONGBUY', '투자의견없음': 'NR'}
 
         self.url = 'http://consensus.hankyung.com'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/51.0.2704.103 Safari/537.36'
         }
+
         self.sdate = _sdate
         self.edate = _edate
         self.now_page = 1
         self.end_page = self.__get_end_page()
-        assert self.end_page >= 1, "end_page가 자연수가 아님"
+        assert self.end_page >= 1, "검색된 날짜에 리포트가 없음"
 
     def __get_end_page(self) -> int:
         params = {
@@ -63,31 +63,28 @@ class HKRequests:
                     html = BeautifulSoup(req.content, 'html.parser')
 
                     for tag in html.select('tbody>tr'):
+                        # 목표증권사가 아니면 reject
                         if (report_corp := tag.select('td')[5].get_text().strip()) not in self.target_corp:
                             continue
 
                         title: str = tag.select_one('div>strong').get_text().strip()
                         raw_code: Match = raw_code_compiler.search(title)
-                        in_raw_code: Match = company_code_compiler.search(raw_code.group()) if raw_code is not None else None
 
-                        if in_raw_code is None:
+                        # 제목에 종목코드가 없으면 reject
+                        if raw_code is None:
                             continue
-                        else:
-                            company_code: str = in_raw_code.group()
 
-                        company_name: str = title[:raw_code.start()].strip() if raw_code is not None else ''
-                        company_name = re.sub(pattern=r'[\[\(]([가-힣\w\s])+[\]\)]', repl='', string=company_name)
+                        in_raw_code: Match = company_code_compiler.search(raw_code.group())
+                        company_code: str = in_raw_code.group() if in_raw_code is not None else '000000'
+                        company_name: str = re.sub(pattern=r'[\[\(]([가-힣\w\s])+[\]\)]', repl='',
+                                                   string=title[:raw_code.start()]).strip()
 
                         date: str = tag.select_one('.first.txt_number').get_text().strip()
                         raw_target_est: str = tag.select('td')[2].get_text().strip().replace(',', '')
                         target_est: int = int(raw_target_est) if raw_target_est.isdigit() else 0
 
-                        if (suggestion := tag.select('td')[3].get_text().strip().upper().replace(' ', '')) in self.suggestion_label:
-                            pass
-                        else:
-                            for raw, cor in self.suggestion_correction:
-                                if suggestion == raw:
-                                    suggestion = cor
+                        if (suggestion := tag.select('td')[3].get_text().strip().upper().replace(' ', '')) in self.suggestion_correction:
+                            suggestion = self.suggestion_correction[suggestion]
 
                         writer: str = tag.select('td')[4].get_text().strip()
                         pdf_link: str = self.url + tag.select_one('a')['href']
@@ -101,6 +98,7 @@ class HKRequests:
                 return reports_list
 
             self.now_page += 1
+
         return reports_list
 
 
