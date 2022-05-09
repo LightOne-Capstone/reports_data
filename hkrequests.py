08e1ad8219a1fa2009e09718261dfbb63720889a
plusjob70 <plusjob70@gmail.com>
2022-05-10 01:44:13 +0900

    modify : date issue

--- hkrequests.py
+++ hkrequests.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 from typing import *
 from fake_useragent import UserAgent
+from pprint import pprint
 
 
 class HKRequests:
@@ -34,9 +35,12 @@ class HKRequests:
         }
 
         self.analyzer = _analyzer
-
         self.last_page = self.__get_last_page()
         assert self.last_page >= 1, "잘못된 요청"
+
+        self.sdate = _sdate
+        self.exceed_range = False
+        self.processed_reports = 0
 
         self.category_file_path = 'corplist.csv'
         self.category_df = self.__get_category_dataframe()
@@ -49,14 +53,14 @@ class HKRequests:
             return response.json().get('last_page', 0)
 
     def request(self, retry_limit=10) -> List[Dict]:
-        ua = UserAgent(verify_ssl=False, use_cache_server=False)
+        ua = UserAgent()
         reports_list = []
 
-        while self.params['page'] <= self.last_page:
-            now_page = self.params['page']
-
+        while not self.exceed_range and (self.params['page'] <= self.last_page):
             for tries in range(retry_limit):
-                print(f'{now_page}/{self.last_page} pages')
+                if self.exceed_range:
+                    break
+
                 try:
                     with requests.get(url=self.url, headers=self.headers, params=self.params) as response:
                         response_data = response.json().get('data', [])
@@ -72,14 +76,19 @@ class HKRequests:
 
                             title = re.sub(pattern=r'[가-힣\w\s]+\(\d{6}\)', repl='',
                                            string=report.get('REPORT_TITLE', '')).strip()
+                            report_date = report.get('REPORT_DATE', '')
                             company_code = report.get('BUSINESS_CODE', '')
                             company_name = report.get('BUSINESS_NAME', '')
-                            report_date = report.get('REPORT_DATE', '')
                             pdf_link = report.get('REPORT_FILEPATH', '')
 
                             # 필수 속성이 없으면 reject
-                            if not all([title, company_code, company_name, report_date, pdf_link]):
+                            if not all([title, report_date, company_code, company_name, pdf_link]):
                                 continue
+
+                            # 날짜가 범위 초과인 경우 검색 중지
+                            if report_date < self.sdate:
+                                self.exceed_range = True
+                                break
 
                             writer = report.get('REPORT_WRITER', '')
                             target_est = int(report.get('TARGET_STOCK_PRICES', 0))
@@ -107,8 +116,10 @@ class HKRequests:
                                            'pdf_link': pdf_link, 'summary': summary}
 
                             # 속성 값이 모두 비어있지 않은 경우만 리스트에 추가
-                            if None not in report.values():
+                            if None not in report_info.values():
                                 reports_list.append(report_info)
+                                self.processed_reports += 1
+                                print(f'{self.processed_reports}개의 리포트 처리')
 
                     self.params['page'] += 1
                     break
