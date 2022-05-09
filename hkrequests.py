eb20d212189ff3b4ecff9aadf1be77cc38e5ce07
plusjob70 <plusjob70@gmail.com>
2022-05-09 23:12:15 +0900

    add : flexible useragent

--- hkrequests.py
+++ hkrequests.py
@@ -1,6 +1,8 @@
-from typing import *
-import pandas as pd
+import re
 import requests
+import pandas as pd
+from typing import *
+from fake_useragent import UserAgent
 
 
 class HKRequests:
@@ -17,6 +19,10 @@ class HKRequests:
                                       '적극매수': 'STRONGBUY', '투자의견없음': 'NR'}
 
         self.url = 'https://markets.hankyung.com/api/consensus/search/report'
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/51.0.2704.103 Safari/537.36'
+        }
         self.params = {
             'page': 1,
             'reportType': 'CO',
@@ -39,67 +45,75 @@ class HKRequests:
         return pd.read_csv(self.category_file_path, encoding='euc-kr', converters={'종목코드': lambda x: str(x)})
 
     def __get_last_page(self) -> int:
-        with requests.get(url=self.url, params=self.params) as response:
+        with requests.get(url=self.url, headers=self.headers, params=self.params) as response:
             return response.json().get('last_page', 0)
 
-    def request(self) -> List[Dict]:
+    def request(self, retry_limit=10) -> List[Dict]:
+        ua = UserAgent(verify_ssl=False, use_cache_server=False)
         reports_list = []
 
         while self.params['page'] <= self.last_page:
-            try:
-                with requests.get(url=self.url, params=self.params) as response:
-                    response_data = response.json().get('data', [])
+            now_page = self.params['page']
 
-                    if type(response_data) is dict:
-                        response_data = response_data.values()
+            for tries in range(retry_limit):
+                print(f'{now_page}/{self.last_page} pages')
+                try:
+                    with requests.get(url=self.url, headers=self.headers, params=self.params) as response:
+                        response_data = response.json().get('data', [])
 
-                    for report in response_data:
-                        # 목표증권사가 아니면 reject
-                        report_corp = report.get('OFFICE_NAME', '').replace(' ', '')
-                        if report_corp not in self.target_corp:
-                            continue
+                        if type(response_data) is dict:
+                            response_data = response_data.values()
 
-                        # 종목 코드가 없으면 reject
-                        company_code = report.get('BUSINESS_CODE', '')
-                        if not company_code:
-                            continue
+                        for report in response_data:
+                            # 목표증권사가 아니면 reject
+                            report_corp = report.get('OFFICE_NAME', '').replace(' ', '')
+                            if report_corp not in self.target_corp:
+                                continue
 
-                        title = report.get('REPORT_TITLE', '')
-                        company_name = report.get('BUSINESS_NAME', '')
-                        report_date = report.get('REPORT_DATE', '')
-                        pdf_link = report.get('REPORT_FILEPATH', '')
-                        target_est = int(report.get('TARGET_STOCK_PRICES', 0))
-                        writer = report.get('REPORT_WRITER', '')
+                            title = re.sub(pattern=r'[가-힣\w\s]+\(\d{6}\)', repl='',
+                                           string=report.get('REPORT_TITLE', '')).strip()
+                            company_code = report.get('BUSINESS_CODE', '')
+                            company_name = report.get('BUSINESS_NAME', '')
+                            report_date = report.get('REPORT_DATE', '')
+                            pdf_link = report.get('REPORT_FILEPATH', '')
 
-                        suggestion = report.get('GRADE_VALUE', '').upper().replace(' ', '')
-                        if suggestion in self.suggestion_correction:
-                            suggestion = self.suggestion_correction[suggestion]
+                            # 필수 속성이 없으면 reject
+                            if not all([title, company_code, company_name, report_date, pdf_link]):
+                                continue
 
-                        category = ''
-                        label = self.category_df.loc[self.category_df['종목코드'] == company_code]['업종']
-                        if not label.empty:
-                            category = label.iloc[0]
+                            writer = report.get('REPORT_WRITER', '')
+                            target_est = int(report.get('TARGET_STOCK_PRICES', 0))
 
-                        # url -> 현재 주가, 기준 날짜, 리포트 요약
-                        self.analyzer.analysis(pdf_link)
-                        current_est: int = self.analyzer.current_est
-                        current_est_date: str = self.analyzer.current_est_date
-                        summary: str = self.analyzer.summary
+                            suggestion = report.get('GRADE_VALUE', '').upper().replace(' ', '')
+                            if suggestion in self.suggestion_correction:
+                                suggestion = self.suggestion_correction[suggestion]
 
-                        report: Dict = {'title': title, 'company_name': company_name, 'company_code': company_code,
-                                        'category': category, 'report_date': report_date, 'suggestion': suggestion,
-                                        'writer': writer, 'report_corp': report_corp, 'target_est': target_est,
-                                        'current_est': current_est, 'current_est_date': current_est_date,
-                                        'pdf_link': pdf_link, 'summary': summary}
+                            # 기업, 업종 맵핑
+                            category = ''
+                            label = self.category_df.loc[self.category_df['종목코드'] == company_code]['업종']
+                            if not label.empty:
+                                category = label.iloc[0]
 
-                        # 속성 값이 모두 비어있지 않은 경우만 리스트에 추가
-                        if None not in report.values():
-                            reports_list.append(report)
+                            # url -> 현재 주가, 기준 날짜, 리포트 요약
+                            self.analyzer.analysis(pdf_link)
+                            current_est: int = self.analyzer.current_est
+                            current_est_date: str = self.analyzer.current_est_date
+                            summary: str = self.analyzer.summary
 
-            except Exception as e:
-                print(e, flush=True)
-                return reports_list
+                            report_info = {'title': title, 'company_name': company_name, 'company_code': company_code,
+                                           'category': category, 'report_date': report_date, 'suggestion': suggestion,
+                                           'writer': writer, 'report_corp': report_corp, 'target_est': target_est,
+                                           'current_est': current_est, 'current_est_date': current_est_date,
+                                           'pdf_link': pdf_link, 'summary': summary}
 
-            self.params['page'] += 1
+                            # 속성 값이 모두 비어있지 않은 경우만 리스트에 추가
+                            if None not in report.values():
+                                reports_list.append(report_info)
+
+                    self.params['page'] += 1
+                    break
+                except Exception as e:
+                    print(f'{e} retrying...')
+                    self.headers['User-Agent'] = ua.random
 
         return reports_list
